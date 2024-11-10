c226e20d10fc4b45037571e620c2c794efacc8a9
balasubramanian1060 <baladevaraj2307@gmail.com>
2024-11-10 17:54:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,174 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import PropTypes from "prop-types";
+// img //
+import snowIcon from "./Component/snowIcon.png";
+import rainIcon from "./Component/rainicon.webp";
+import clearIcon from "./Component/clearIcon.png";
+import drizzleIcon from "./Component/drizzleIcon.png";
+import cloudIcon from "./Component/cloudIcon.webp";
+
+
+
+const WeatherDetails = ({ icon, temp, city, country, lat, log, wind, humidity }) => {
+  return (
+    <>
+      <div className='image'>
+        <img src={icon} alt='sunny'></img>
+      </div>
+      <div className='temp'> {temp}*C</div>
+      <div className='location'>{city}</div>
+      <div className='country'>{country}</div>
+      <div className='cord'>
+        <div>
+          <span className='lat'>Latitude</span>
+          <span>{lat}</span>
+        </div>
+        <div>
+          <span className='log'>Longitude</span>
+          <span>{log}</span>
+        </div>
+      </div>
+      <div className='data-container'>
+        <div className='element'>
+          <svg className='humidityicon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path d="M269.5 69.9c11.1-7.9 25.9-7.9 37 0C329 85.4 356.5 96 384 96c26.9 0 55.4-10.8 77.4-26.1c0 0 0 0 0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 149.7 417 160 384 160c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4C42.8 92.6 61 83.5 75.3 71.6c11.1-9.5 27.3-10.1 39.2-1.7c0 0 0 0 0 0C136.7 85.2 165.1 96 192 96c27.5 0 55-10.6 77.5-26.1zm37 288C329 373.4 356.5 384 384 384c26.9 0 55.4-10.8 77.4-26.1c0 0 0 0 0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 437.7 417 448 384 448c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4c18.1-4.2 36.2-13.3 50.6-25.2c11.1-9.4 27.3-10.1 39.2-1.7c0 0 0 0 0 0C136.7 373.2 165.1 384 192 384c27.5 0 55-10.6 77.5-26.1c11.1-7.9 25.9-7.9 37 0zm0-144C329 229.4 356.5 240 384 240c26.9 0 55.4-10.8 77.4-26.1c0 0 0 0 0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 293.7 417 304 384 304c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4c18.1-4.2 36.2-13.3 50.6-25.2c11.1-9.5 27.3-10.1 39.2-1.7c0 0 0 0 0 0C136.7 229.2 165.1 240 192 240c27.5 0 55-10.6 77.5-26.1c11.1-7.9 25.9-7.9 37 0z" /></svg>
+          <div className='data'>
+            <div className='humidity-percent'>{humidity}%</div>
+            <div className='text'>Humidity</div>
+          </div>
+        </div>
+        <div className='element'>
+          <svg className='windicon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 32c0 17.7 14.3 32 32 32l32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128c-17.7 0-32 14.3-32 32s14.3 32 32 32l320 0c53 0 96-43 96-96s-43-96-96-96L320 0c-17.7 0-32 14.3-32 32zm64 352c0 17.7 14.3 32 32 32l32 0c53 0 96-43 96-96s-43-96-96-96L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0c-17.7 0-32 14.3-32 32zM128 512l32 0c53 0 96-43 96-96s-43-96-96-96L32 320c-17.7 0-32 14.3-32 32s14.3 32 32 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0c-17.7 0-32 14.3-32 32s14.3 32 32 32z" /></svg>
+          <div className='data'>
+            <div className='wind-percent'>{wind} km/h</div>
+            <div className='text'>Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+WeatherDetails.propTypes={
+  icon: PropTypes.string.isRequired,
+  temp: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  humidity: PropTypes.number.isRequired,
+  wind: PropTypes.number.isRequired,
+  lat: PropTypes.number.isRequired,
+  log: PropTypes.number.isRequired,
+}
 
 function App() {
+
+  const [text, setText] = useState("London");
+
+  const [icon, setIcon] = useState(clearIcon);
+  const [temp, setTemp] = useState(0);
+  const [city, setCity] = useState("London");
+  const [country, setCountry] = useState("GB");
+  const [lat, setLat] = useState(0);
+  const [log, setLog] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
+  const [error, setError] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+  const [cityNotFound, setCityNotFound] = useState(false);
+
+
+  const weatherIconMap = {
+    "01d": clearIcon,
+    "01n": clearIcon,
+    "02d": cloudIcon,
+    "02n": cloudIcon,
+    "03d": drizzleIcon,
+    "03n": drizzleIcon,
+    "04d": drizzleIcon,
+    "04n": drizzleIcon,
+    "09d": rainIcon,
+    "09n": rainIcon,
+    "10d": rainIcon,
+    "10n": rainIcon,
+    "13d": snowIcon,
+    "13n": snowIcon,
+
+  };
+
+
+  const search = async () => {
+    setLoading(true);
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=74c519349af3ed9075a5239444180067&units=metric`
+
+    try {
+      let res = await fetch(url);
+      let data = await res.json()
+
+      if (data.cod === "404") {
+        console.log("city not found");
+        setCityNotFound(true);
+        setLoading(false);
+        return;
+      };
+
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setLat(data.coord.lat);
+      setLog(data.coord.lon);
+      const weatherIconCode = data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconCode] || clearIcon);
+      setCityNotFound(false);
+
+    } catch (error) {
+      console.log("An error occurred", error.message);
+      setError("An error occurred while fetching weather data.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCity = (e) => {
+    setText(e.target.value);
+  };
+  const handlekeydown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  useEffect(()=> {
+    search();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='input-container'>
+          <input type='text' className='cityinput' placeholder='Search City' onChange={handleCity} value={text} onKeyDown={handlekeydown} />
+          <div className='search-icon ' onClick={() => { search() }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
+          </div>
+        </div>
+        
+
+        {loading && <div className='loading-message'>Loading...</div>}
+        {error && <div className='error-message'>{error}</div>}
+       {cityNotFound && <div className='city-not-found'> City Not Found</div>}
+
+       {! loading && !cityNotFound && <WeatherDetails icon={icon} temp={temp} city={city} country={country} lat={lat} log={log} humidity={humidity} wind={wind} />}
+
+
+      </div>
     </div>
   );
-}
+};
 
 export default App;
